c0771805ed88322bfa36f816495811025c3178c4
EnjoyTheNoise <jankos.pdp@gmail.com>
2017-11-23 21:51:29 +0100

    snapshot

---
+++ src/animationWindow/Kwadrat.java
@@ -0,0 +1,17 @@
+package animationWindow;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+
+public class Kwadrat extends Figure{
+
+	public Kwadrat(Graphics2D buffer, int delay, int width, int height) {
+		super(buffer,delay,width,height);
+		shape = new Rectangle2D.Float(0,0,width,height);
+		aft = new AffineTransform();
+		area = new Area(shape);
+
+	}
+}
