6cb962788415765766f2e4c888f221df3231d8a6
EnjoyTheNoise <jankos.pdp@gmail.com>
2017-11-23 21:51:29 +0100

    fixed pause, bg always white, ui scales with window resize (anim. not)

---
+++ src/animationWindow/Elipsa.java
@@ -0,0 +1,17 @@
+package animationWindow;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+
+public class Elipsa extends Figure {
+	
+	public Elipsa(Graphics2D buffer, int delay, int width, int height){
+		super(buffer,delay,width,height);
+		shape = new Ellipse2D.Double(0, 0, width, height);
+		aft = new AffineTransform();
+		area = new Area(shape);
+	}
+
+}
